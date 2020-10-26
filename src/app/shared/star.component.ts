13b75a1b05ba8b5a70659bc5e884df910c8c7643
sarahlia <sarah.l.nainggolan@gmail.com>
2020-10-25 20:28:44 -0500

    finished passing data FROM a component: when the star rating is clicked, the page title changes to display the message that the rating was clicked.

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnChanges} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, Output} from '@angular/core';
 
 @Component({
   selector: 'pm-star',
@@ -8,12 +8,14 @@ import {Component, Input, OnChanges} from '@angular/core';
 export class StarComponent implements OnChanges {
   @Input() rating: number;
   starWidth: number;
+  @Output() ratingClicked: EventEmitter<string> =
+    new EventEmitter<string>();
 
   ngOnChanges(): void {
     this.starWidth = this.rating * 75 / 5;
   }
 
   onClick(): void {
-    console.log(`The rating ${this.rating} was clicked!`);
+    this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
   }
 }
