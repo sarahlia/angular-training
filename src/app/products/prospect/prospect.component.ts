1da38b3f537f57efee33beeca93af11cdbe9a6a2
sarahlia <sarah.l.nainggolan@gmail.com>
2020-12-02 12:33:56 -0600

    got field-sort class only to be sorted in order

--- src/app/products/prospect/prospect.component.ts
+++ src/app/products/prospect/prospect.component.ts
@@ -10,6 +10,7 @@ export class ProspectComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.mySortFunction();
   }
 
   onClick(): void {
@@ -18,5 +19,26 @@ export class ProspectComponent implements OnInit {
 
   numSequence(n: number): Array<number> {
     return Array(n);
+  }
+
+  mySortFunction(): void {
+    let elems = document.getElementsByClassName('field-sort');
+
+    // convert NodeList to array
+    let elemsArr = Array.from(elems);
+
+    // perform sort
+    elemsArr.sort((a, b) => {
+      return Number(a.innerHTML) - Number(b.innerHTML);
+    });
+
+    // join the array back into HTML
+    let output = '';
+    for (let i = 0; i < elemsArr.length; i++) {
+      output += elemsArr[i].outerHTML;
+    }
+
+    // append output to div 'result'
+    document.getElementById('result').innerHTML = output;
   }
 }
